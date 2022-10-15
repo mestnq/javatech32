a574dccf06c279bc7bf7035dd2fe99f0fa62f6db
Kseniya <kseniya-ignateva-2002@mail.ru>
2022-10-15 23:13:48 +0500

    Showing a list of files and folders on a computer
    Made a Servlet + JSP application showing a list of files and folders on a computer

---
+++ src/main/java/com/example/demoweb/FileManagerServlet.java
@@ -0,0 +1,81 @@
+package com.example.demoweb;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/")
+public class FileManagerServlet extends HttpServlet {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getParameter("path");
+
+        if (path == null) {
+            path = System.getProperty("user.dir");
+        }
+
+        File currentPath = new File(path);
+
+        if (currentPath.isDirectory()) {
+            viewFiles(req, currentPath);
+            req.setAttribute("date", dateFormat.format(new Date()));
+            req.setAttribute("currentPath", path);
+            req.getRequestDispatcher("fileManager.jsp").forward(req, resp);
+        } else {
+            downloadFile(resp, currentPath);
+        }
+    }
+
+    private void downloadFile(HttpServletResponse resp, File file) throws IOException {
+        resp.setContentType("text/plain");
+        resp.setHeader("Content-disposition", "attachment; filename=" + file.getName());
+
+        try (InputStream in = new FileInputStream(file); OutputStream out = resp.getOutputStream()) {
+            byte[] buffer = new byte[1048];
+
+            int numBytesRead;
+            while ((numBytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, numBytesRead);
+            }
+        }
+    }
+
+    private void viewFiles(HttpServletRequest req, File currentPath) {
+        File[] allFiles = currentPath.listFiles();
+        if (allFiles == null) {
+            return;
+        }
+        List<File> directories = new ArrayList<>();
+        List<File> files = new ArrayList<>();
+        for (File file : allFiles) {
+            (file.isDirectory() ? directories : files).add(file);
+        }
+        req.setAttribute("files", files);
+        req.setAttribute("directories", directories);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+}
---
+++ src/main/webapp/fileManager.jsp
@@ -0,0 +1,27 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Ксения
+  Date: 15.10.2022
+  Time: 20:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>File Manager</title>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+</head>
+<body>
+<h5>${date}</h5>
+<h1>${currentPath}</h1>
+<ul>
+    <b><a href="?path=${currentPath.substring(0, currentPath.lastIndexOf("\\") + (currentPath.lastIndexOf("\\") != currentPath.indexOf("\\") ? 0 : 1))}">Вверх</a></b><br><br>
+    <c:forEach var="directory" items="${directories}">
+        <a href="?path=${directory.getAbsolutePath()}">${directory.getName()}/</a><br>
+    </c:forEach>
+    <c:forEach var="file" items="${files}">
+        <a href="?path=${file.getAbsolutePath()}">${file.getName()}</a><br>
+    </c:forEach>
+</ul>
+</body>
+</html>
